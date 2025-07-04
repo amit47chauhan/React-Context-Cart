d0d299bfed29d4c77f84409538b8d7163c906087
amit47chauhan <chauhanamit.ak47@gmail.com>
2025-07-04 23:50:31 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'React-Context-Cart', // <-- Set this to your repo name for GitHub Pages
+  base: '/React-Context-Cart/', // <-- Set this to your repo name for GitHub Pages
 })
